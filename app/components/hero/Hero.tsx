9aff8aded5ed0fa6b0f3f9ff39223a96f58c22a5
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-08-21 16:26:43 +0100

    feat: added basic hero to home page

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,26 +1,33 @@
+"use client";
+
+import StarsSvg from "@/app/svg_components/Stars";
+import { motion } from "framer-motion";
+
 const Hero: React.FC = () => {
   return (
-    <></>
-    // <div className='main-page-container'>
+    <>
+      {/* // <div className='main-page-container'>
     //   {!isMobile ? <Nav scrollToSection={scrollToSection} /> : null}
     //   <img src='images/gradient.webp' alt='' className='hero-img' />
     //   {isMobile ? (
-    //     <div>
-    //       <Burger />{" "}
-    //     </div>
-    //   ) : null}
-    //   <div className='hi-container'>
-    //     <div className='hi'>Hi, Im Jacob</div>
-    //     <div className='hi-lower'>I make things for the web.</div>
-    //   </div>
-    //   <StarsSvg className='overlay-svg' />
+        <div>
+          <Burger />{" "}
+        </div>
+      ) : null} */}
+      <div className='hi-container'>
+        <div className='hi'>Hi, Im Jacob</div>
+        <div className='hi-lower'>I make things for the web.</div>
+      </div>
+      {/* <StarsSvg className='overlay-svg' /> */}
 
-    //   <motion.div
-    //     initial={"offscreen"}
-    //     whileInView={"onscreen"}
-    //     variants={textAnimate}
-    //     viewport={{ once: true, amount: 0.1 }}
-    //     className='contents-container'></motion.div>
-    // </div>
+      <motion.div
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        // variants={textAnimate}
+        viewport={{ once: true, amount: 0.1 }}
+        className='contents-container'></motion.div>
+    </>
   );
 };
+
+export default Hero;
