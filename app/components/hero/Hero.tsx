39926abbe161659c5e1db35b8a609d28c91a2833
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-08-21 16:12:44 +0100

    feat: created Home component

---
+++ app/components/hero/Hero.tsx
@@ -0,0 +1,26 @@
+const Hero: React.FC = () => {
+  return (
+    <></>
+    // <div className='main-page-container'>
+    //   {!isMobile ? <Nav scrollToSection={scrollToSection} /> : null}
+    //   <img src='images/gradient.webp' alt='' className='hero-img' />
+    //   {isMobile ? (
+    //     <div>
+    //       <Burger />{" "}
+    //     </div>
+    //   ) : null}
+    //   <div className='hi-container'>
+    //     <div className='hi'>Hi, Im Jacob</div>
+    //     <div className='hi-lower'>I make things for the web.</div>
+    //   </div>
+    //   <StarsSvg className='overlay-svg' />
+
+    //   <motion.div
+    //     initial={"offscreen"}
+    //     whileInView={"onscreen"}
+    //     variants={textAnimate}
+    //     viewport={{ once: true, amount: 0.1 }}
+    //     className='contents-container'></motion.div>
+    // </div>
+  );
+};
