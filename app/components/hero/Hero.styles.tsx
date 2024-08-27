73295ebc0cf40137b0cfded0f1efaa1bb157ab0a
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-08-27 17:14:13 +0100

    style: added responsive styles to Hero

--- app/components/hero/Hero.styles.tsx
+++ app/components/hero/Hero.styles.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
-import { DarkerGrotesque } from "@/app/fonts";
+import { media } from "@/app/utils/media";
 
 export const HeroMain = styled.div`
   padding-bottom: 100px;
   position: relative;
   z-index: 1;
+
+  ${media.mobile`
+    padding-bottom: 40px;
+
+  `}
 `;
 
 export const HeroImage = styled.img`
@@ -29,10 +34,19 @@ export const HeroContent = styled.div`
   font-size: 100px;
   color: rgb(45, 255, 167);
   text-shadow: 0 0 6px rgb(45, 255, 167);
+
+  ${media.mobile`
+    font-size: 3em;
+    padding-top: 150px;
+  `}
 `;
 
 export const HeroContentLower = styled.div`
   font-size: 40px;
   color: rgb(198, 198, 198);
   text-shadow: 0 0 6px #cccfcabf;
+
+  ${media.mobile`
+    font-size: 1em;
+  `}
 `;
